aee7d1e1d8bfc51900e59f02b153d9acd909bed0
Ron-Hadad <ron101200@gmail.com>
2024-09-16 22:40:31 +0300

    frontend support messages formats

---
+++ frontend/my-app/src/components/AddNewFormat.tsx
@@ -0,0 +1,87 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { useMaliciousFormat } from "./MaliciousListHook";
+
+const AddNewFormat = () => {
+  const [newMaliciousFormat, setNewMaliciousFormat] = useState({
+    sourceEmail: "",
+    sourcePhone: "",
+    message: "",
+    subject: "",
+  });
+  const {
+    maliciousFormats,
+    handleEditApiCall,
+    handleDeleteApiCall,
+    handleCreateApiCall,
+    loading,
+  } = useMaliciousFormat();
+
+  const handleAddNewFormat = () => {
+    if (newMaliciousFormat.message === "") {
+      alert("please add message");
+      return;
+    }
+    handleCreateApiCall({ ...newMaliciousFormat, id: "0" }); //dummy id
+    setNewMaliciousFormat({
+      sourceEmail: "",
+      sourcePhone: "",
+      message: "",
+      subject: "",
+    }); // Clear the form after adding
+  };
+  const handleNewItemChange = (field: string, value: string) => {
+    setNewMaliciousFormat((prevFields) => ({ ...prevFields, [field]: value }));
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flex: "2",
+        gap: 2,
+        mt: 4,
+        paddingRight: "40px",
+      }}
+    >
+      <Typography variant="h6">Add New format</Typography>
+      <TextField
+        label="sourceEmail"
+        value={newMaliciousFormat.sourceEmail}
+        onChange={(e) => handleNewItemChange("sourceEmail", e.target.value)}
+        fullWidth
+      />
+      <TextField
+        label="sourcePhone"
+        value={newMaliciousFormat.sourcePhone}
+        onChange={(e) => handleNewItemChange("sourcePhone", e.target.value)}
+        fullWidth
+      />
+      <TextField
+        label="message"
+        value={newMaliciousFormat.message}
+        onChange={(e) => handleNewItemChange("message", e.target.value)}
+        fullWidth
+      />
+      <TextField
+        label="subject"
+        value={newMaliciousFormat.subject}
+        onChange={(e) => handleNewItemChange("subject", e.target.value)}
+        fullWidth
+      />
+      <Button
+        variant="contained"
+        startIcon={<AddIcon />}
+        onClick={handleAddNewFormat}
+        // disabled={!newMaliciousFormat.message || !newMaliciousFormat.subject} // Disable button if inputs are empty
+      >
+        Add Item
+      </Button>
+    </Box>
+  );
+};
+
+export default AddNewFormat;
--- frontend/my-app/src/components/CampaignPage.tsx
+++ frontend/my-app/src/components/CampaignPage.tsx
@@ -1,48 +1,24 @@
 import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import api from "../services/api";
-import "../styles/CampaignPage.css"; // Import the CSS file
-import { Contact } from "../types/appTypes";
 import BackToMainpageButton from "./BackToMainpageButton";
+import AddMaliciousFormat from "./AddMaliciousFormat";
+import MaliciousList from "./MaliciousList";
+import "./../styles/CampaignPage.css"; // Import the CSS
+import AddNewFormat from "./AddNewFormat";
+import SendMessagesButton from "./SendMessagesButton";
 
 const CampaignPage: React.FC = () => {
-  const { state } = useLocation();
-  const { contacts }: { contacts: Contact[] } = state;
-  const [subject, setSubject] = useState("");
-  const [body, setBody] = useState("");
-  const navigate = useNavigate();
-
-  const handleSendEmail = async () => {
-    if (!subject || !body) {
-      alert("Subject and body are required.");
-      return;
-    }
-    try {
-      await api.post("/campaign/send", { contacts, subject, body });
-      alert("Emails sent successfully!");
-      navigate("/main");
-    } catch (error) {
-      console.error("Error sending email:", error);
-      alert("Failed to send emails. Please try again.");
-    }
-  };
-
+  const [choosenFormatId, setChoosenFormatId] = useState<string | null>(null);
   return (
     <div>
       <BackToMainpageButton />
-      <h1>Create Email Campaign</h1>
-      <input
-        type="text"
-        placeholder="Subject"
-        value={subject}
-        onChange={(e) => setSubject(e.target.value)}
-      />
-      <textarea
-        placeholder="Email body"
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-      />
-      <button onClick={handleSendEmail}>Send</button>
+      <div className="malicious-options">
+        <MaliciousList
+          choosenFormatId={choosenFormatId}
+          setChoosenFormatId={setChoosenFormatId}
+        />
+        <AddNewFormat />
+        <SendMessagesButton choosenFormatId={choosenFormatId} />
+      </div>
     </div>
   );
 };
